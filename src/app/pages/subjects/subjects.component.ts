330e892d49dd35a895ee794eae7275bb9dfa5c70
Yura Myroniak <yura.myroniak20@gmail.com>
2026-02-01 18:21:36 +0200

    feat: implement subjects list and add form

--- src/app/pages/subjects/subjects.component.ts
+++ src/app/pages/subjects/subjects.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { SubjectsService } from '../../core/services/subjects.service';
 
 @Component({
   selector: 'app-subjects',
-  imports: [],
   templateUrl: './subjects.component.html',
-  styleUrl: './subjects.component.scss'
 })
 export class SubjectsComponent {
+  title = '';
+  teacher = '';
+  subjects$ = this.subjectsService.subjects$;
 
+  constructor(private subjectsService: SubjectsService) {}
+
+  add() {
+    if (!this.title.trim()) return;
+    this.subjectsService.addSubject({
+      title: this.title.trim(),
+      teacher: this.teacher.trim() || undefined,
+    });
+    this.title = '';
+    this.teacher = '';
+  }
 }
